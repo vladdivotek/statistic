00c38bd78df58d16993d677450b3be281ba8125d
vladislaw.frontend <vladislaw.frontend@gmail.com>
2024-03-13 12:01:54 +0200

    Create service fetching data

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Statistic;
+use App\Services\FetchData;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +19,7 @@ class StatisticController extends Controller
 
     public function generate()
     {
-        if (DB::table(Statistic::TABLE)->count() == 0) $mergeItems = $this->fetchData();
+        if (DB::table(Statistic::TABLE)->count() == 0) $mergeItems = FetchData::getData();
 
         $statisticItems = DB::table(Statistic::TABLE)
             ->select('ad_id', 'impressions', 'clicks', 'unique_clicks', 'leads', 'conversion', 'roi')
@@ -37,46 +38,5 @@ class StatisticController extends Controller
             ->get();
 
         return response()->json(['searchResult' => $searchResult]);
-    }
-
-    public function fetchData()
-    {
-        $endpoint1Items = $endpoint2Items = $mergeItems = null;
-
-        try {
-            $responseEndpoint1 = Http::get('https://submitter.tech/test-task/endpoint1.json');
-            $endpoint1Items = $responseEndpoint1->json();
-
-            $responseEndpoint2 = Http::get('https://submitter.tech/test-task/endpoint2.json');
-            $endpoint2Items = $responseEndpoint2->json()['data']['list'];
-        } catch (Exception $e) {
-            return response()->json('Error while fetching data from resources: ' . $e->getMessage());
-        }
-
-        if (count($endpoint1Items) > 0 && count($endpoint2Items) > 0) {
-            foreach ($endpoint1Items as $endpoint1Item) {
-                foreach ($endpoint2Items as $endpoint2Item) {
-                    if ($endpoint1Item['name'] == $endpoint2Item['dimensions']['ad_id']) {
-                        $mergeItems[] = [
-                            'ad_id' => $endpoint2Item['dimensions']['ad_id'],
-                            'impressions' => $endpoint2Item['metrics']['impressions'],
-                            'clicks' => $endpoint1Item['clicks'],
-                            'unique_clicks' => $endpoint1Item['unique_clicks'],
-                            'leads' => $endpoint1Item['leads'],
-                            'conversion' => $endpoint2Item['metrics']['conversion'],
-                            'roi' => $endpoint1Item['roi']
-                        ];
-                    }
-                }
-            }
-
-            usort($mergeItems, function($a, $b) {
-                return $b['impressions'] - $a['impressions'];
-            });
-
-            Statistic::query()->insert($mergeItems);
-        } else {
-            return response()->json('Error while processing data from resources.');
-        }
     }
 }
---
+++ app/Services/FetchData.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Statistic;
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+class FetchData
+{
+    public static function getData()
+    {
+        $endpoint1Items = $endpoint2Items = $mergeItems = null;
+        $endpoint1Url = env('ENDPOINT_1');
+        $endpoint2Url = env('ENDPOINT_2');
+
+        if (!$endpoint1Url || !$endpoint2Url) return response()->json('Endpoints urls doesnt exist');
+
+        try {
+            $responseEndpoint1 = Http::get(env('ENDPOINT_1'));
+            $endpoint1Items = $responseEndpoint1->json();
+
+            $responseEndpoint2 = Http::get('https://submitter.tech/test-task/endpoint2.json');
+            $endpoint2Items = $responseEndpoint2->json()['data']['list'];
+        } catch (Exception $e) {
+            return response()->json('Error while fetching data from resources: ' . $e->getMessage());
+        }
+
+        if (count($endpoint1Items) > 0 && count($endpoint2Items) > 0) {
+            foreach ($endpoint1Items as $endpoint1Item) {
+                foreach ($endpoint2Items as $endpoint2Item) {
+                    if ($endpoint1Item['name'] == $endpoint2Item['dimensions']['ad_id']) {
+                        $mergeItems[] = [
+                            'ad_id' => $endpoint2Item['dimensions']['ad_id'],
+                            'impressions' => $endpoint2Item['metrics']['impressions'],
+                            'clicks' => $endpoint1Item['clicks'],
+                            'unique_clicks' => $endpoint1Item['unique_clicks'],
+                            'leads' => $endpoint1Item['leads'],
+                            'conversion' => $endpoint2Item['metrics']['conversion'],
+                            'roi' => $endpoint1Item['roi']
+                        ];
+                    }
+                }
+            }
+
+            usort($mergeItems, function($a, $b) {
+                return $b['impressions'] - $a['impressions'];
+            });
+
+            Statistic::query()->insert($mergeItems);
+        } else {
+            return response()->json('Error while processing data from resources.');
+        }
+
+        return $mergeItems;
+    }
+}
